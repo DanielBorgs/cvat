d20ef9628fb83c0c797230a3a66092e066e597b5
Duarte Pereira da Cruz <duarte.cruz@tecnico.ulisboa.pt>
2026-05-27 15:58:41 +0100

    feat: add tests

--- tests/cypress/e2e/issues_prs2/issue_9578_mirror_and_bbox_edit_mode.js
+++ tests/cypress/e2e/issues_prs2/issue_9578_mirror_and_bbox_edit_mode.js
@@ -30,6 +30,16 @@ context('Feature: Mirroring and BBox Edit Mode', () => {
         ],
     };
 
+    const createPolylinePoints = {
+        type: 'Shape',
+        labelName: labelName,
+        pointsMap: [
+            { x: 600, y: 300 },
+            { x: 700, y: 400 },
+            { x: 600, y: 500 },
+        ],
+    };
+
     before(() => {
         cy.prepareUserSession();
         cy.openTaskJob(taskName);
@@ -82,6 +92,24 @@ context('Feature: Mirroring and BBox Edit Mode', () => {
 
             cy.get('body').type('s');
         });
+
+        it('Rotate the polygon using the BBox rotation handle', () => {
+            cy.get('#cvat_canvas_shape_1').click();
+            cy.get('body').type('s');
+
+            cy.get('#cvat_canvas_shape_1').invoke('attr', 'points').then((originalPoints) => {
+                cy.get('.svg_select_points_rot').trigger('mousedown', { button: 0 });
+
+                cy.get('.cvat_canvas_wrapper').trigger('mousemove', { clientX: 450, clientY: 200 });
+                cy.get('.cvat_canvas_wrapper').trigger('mouseup');
+
+                cy.get('#cvat_canvas_shape_1').invoke('attr', 'points').should((rotatedPoints) => {
+                    expect(rotatedPoints).not.to.equal(originalPoints);
+                });
+            });
+
+            cy.get('body').type('s');
+        });
     });
 
     describe(`Testing PR "${issueId}": Mirroring Math and History`, () => {
@@ -111,6 +139,164 @@ context('Feature: Mirroring and BBox Edit Mode', () => {
             cy.get('#cvat_canvas_shape_1').invoke('attr', 'points').then((originalPoints) => {
                 cy.get('#cvat-objects-sidebar-state-item-1').find('.ant-dropdown-trigger').click({ force: true });
                 cy.get('.cvat-object-item-menu-mirror-vertical').click();
+
+                cy.get('#cvat_canvas_shape_1').invoke('attr', 'points').should('not.equal', originalPoints);
+            });
+        });
+
+        it('Mirror the polygon horizontally via the Object Item Menu', () => {
+            cy.get('#cvat_canvas_shape_1').invoke('attr', 'points').then((originalPoints) => {
+                cy.get('#cvat-objects-sidebar-state-item-1').find('.ant-dropdown-trigger').click({ force: true });
+
+                cy.get('.cvat-object-item-menu-mirror-horizontal').click();
+
+                cy.get('#cvat_canvas_shape_1').invoke('attr', 'points').should('not.equal', originalPoints);
+            });
+        });
+    });
+
+    describe(`Testing PR "${issueId}": Polyline Support`, () => {
+        it('Create a polyline shape', () => {
+            cy.createPolyline(createPolylinePoints);
+        });
+
+        it('Scale the polyline using BBox edit mode', () => {
+            cy.get('#cvat_canvas_shape_3').click();
+            cy.get('body').type('s');
+
+            cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').then((originalPoints) => {
+                cy.get('.svg_select_points_rb').trigger('mousedown', { button: 0 });
+                cy.get('.cvat_canvas_active_bbox').trigger('mousemove', { clientX: 750, clientY: 550 });
+                cy.get('.cvat_canvas_active_bbox').trigger('mouseup');
+
+                cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').should((scaledPoints) => {
+                    expect(scaledPoints).not.to.equal(originalPoints);
+                });
+            });
+            cy.get('body').type('s');
+        });
+
+        it('Mirror the polyline vertically', () => {
+            cy.get('#cvat_canvas_shape_3').click();
+            cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').then((originalPoints) => {
+                cy.get('body').type('{shift}v');
+                cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').should('not.equal', originalPoints);
+            });
+        });
+
+        it('Scale the polyline using the right-middle BBox handle', () => {
+            cy.get('#cvat_canvas_shape_3').click();
+            cy.get('body').type('s');
+
+            cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').then((originalPoints) => {
+                cy.get('.svg_select_points_r').trigger('mousedown', { button: 0 });
+                cy.get('.cvat_canvas_active_bbox').trigger('mousemove', { clientX: 750, clientY: 400 });
+                cy.get('.cvat_canvas_active_bbox').trigger('mouseup');
+
+                cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').should((scaledPoints) => {
+                    expect(scaledPoints).not.to.equal(originalPoints);
+                });
+            });
+            cy.get('body').type('s');
+        });
+
+        it('Rotate the polyline using the BBox rotation handle', () => {
+            cy.get('#cvat_canvas_shape_3').click();
+            cy.get('body').type('s');
+
+            cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').then((originalPoints) => {
+                cy.get('.svg_select_points_rot').trigger('mousedown', { button: 0 });
+                cy.get('.cvat_canvas_wrapper').trigger('mousemove', { clientX: 550, clientY: 250 });
+                cy.get('.cvat_canvas_wrapper').trigger('mouseup');
+
+                cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').should((rotatedPoints) => {
+                    expect(rotatedPoints).not.to.equal(originalPoints);
+                });
+            });
+            cy.get('body').type('s');
+        });
+
+        it('Mirror the polyline horizontally via shortcut "Shift+H"', () => {
+            cy.get('#cvat_canvas_shape_3').click();
+            cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').then((originalPoints) => {
+                cy.get('body').type('{shift}h');
+                cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').should('not.equal', originalPoints);
+            });
+        });
+
+        it('Mirror the polyline vertically via the Object Item Menu', () => {
+            cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').then((originalPoints) => {
+                cy.get('#cvat-objects-sidebar-state-item-3').find('.ant-dropdown-trigger').click({ force: true });
+
+                cy.get('.cvat-object-item-menu-mirror-vertical').click();
+
+                cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').should('not.equal', originalPoints);
+            });
+        });
+
+        it('Mirror the polyline horizontally via the Object Item Menu', () => {
+            cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').then((originalPoints) => {
+                cy.get('#cvat-objects-sidebar-state-item-3').find('.ant-dropdown-trigger').click({ force: true });
+
+                cy.get('.cvat-object-item-menu-mirror-horizontal').click();
+
+                cy.get('#cvat_canvas_shape_3').invoke('attr', 'points').should('not.equal', originalPoints);
+            });
+        });
+    });
+
+    describe(`Testing PR "${issueId}": Persistence`, () => {
+        it('Save the job, reload the page, and verify the shapes retain modifications', () => {
+            cy.get('#cvat_canvas_shape_1').invoke('attr', 'points').then((pointsBeforeSave) => {
+
+                cy.saveJob('Save');
+
+                cy.reload();
+                cy.get('.cvat-canvas-container').should('exist');
+
+                cy.get('#cvat_canvas_shape_1').invoke('attr', 'points').should((pointsAfterReload) => {
+                    expect(pointsAfterReload).to.equal(pointsBeforeSave);
+                });
+            });
+        });
+    });
+
+    describe(`Testing PR "${issueId}": Customizing Shortcuts`, () => {
+        it('Change BBox Edit Mode and Mirroring shortcuts in settings', () => {
+            cy.get('.cvat-header-menu-button').click();
+            cy.get('.cvat-header-menu-settings').click();
+
+            cy.get('.ant-tabs-tab').contains('Shortcuts').click();
+
+            cy.get('.cvat-shortcut-table').contains('Toggle edit mode').parent().within(() => {
+                cy.get('.cvat-shortcut-key').click();
+                cy.get('body').type('b');
+            });
+
+            cy.get('.cvat-shortcut-table').contains('Mirror horizontal').parent().within(() => {
+                cy.get('.cvat-shortcut-key').click();
+                cy.get('body').type('{shift}m');
+            });
+
+            cy.get('.cvat-settings-modal').find('.ant-modal-close').click();
+        });
+
+        it('Verify the new shortcut toggles BBox Edit Mode', () => {
+            cy.get('#cvat_canvas_shape_1').click();
+
+            cy.get('body').type('s');
+            cy.get('.cvat_canvas_active_bbox').should('not.exist');
+
+            cy.get('body').type('b');
+            cy.get('.cvat_canvas_active_bbox').should('exist');
+            cy.get('body').type('b');
+        });
+
+        it('Verify the new shortcut triggers Horizontal Mirroring', () => {
+            cy.get('#cvat_canvas_shape_1').click();
+            cy.get('#cvat_canvas_shape_1').invoke('attr', 'points').then((originalPoints) => {
+
+                cy.get('body').type('{shift}m');
 
                 cy.get('#cvat_canvas_shape_1').invoke('attr', 'points').should('not.equal', originalPoints);
             });
